947a706a49804db896add66c14ac154b79826bec
Michael Schmatz <schmatz@umich.edu>
2013-05-20 16:17:33 -0700

    Further progress on tiff2hdf

--- tiff2hdf/tiff2hdf.py
+++ tiff2hdf/tiff2hdf.py
@@ -23,9 +23,14 @@
 # NEMALOAD
 # nemaload.com
 
-import sys, argparse, os, ctypes, numpy, h5py, Image, subprocess
+#To install PIL,
+#http://athenageek.wordpress.com/2009/06/09/easy_install-pil-not-so-easy/
 
-from wand.image import Image
+import sys, argparse, os, ctypes, numpy, h5py
+
+from wand.image import Image as wandImage
+from scipy import misc
+import Image
 
 class imageConversion:
 	def __init__(self):
@@ -47,12 +52,14 @@ class imageConversion:
 	#REQUIRES: self.setImageLocation must already be called
 	#MODIFIES: self
 	#EFFECTS: Gets various details about the input image
-		with Image(filename=self.location) as img:
+		with wandImage(filename=self.location) as img:
 			self.width = img.width
 			self.height = img.height
 			self.bitdepth = img.depth
 			self.format = img.format
 			self.colormode = img.type
+			# using PIL, seek until EOF error is experienced, and set numImages to how many
+			# 	iterations it got through before that
 			self.numImages = img.frame_num #really unsure about this one
 
 
@@ -82,34 +89,78 @@ class imageConversion:
 	#REQUIRES: self.getImageDetails must have already been called
 	#EFFECTS: Return true if image is grayscale, false otherwise
 
-		if self.type is 'grayscale':
-			return true
-		else:
-			return false
-	def colorCanBeConverted(self):
-	#REQUIRES: self.getImageDetails must have already been called
-	#EFFECTS: returns true if image can be converted to grayscale,
-	# 	false otherwise.
-		if self.type in ('truecolor', 'truecolormatte'):
+    	if self.type is 'grayscale':
 			return true
 		else:
 			return false
 
-	def setBitDepth(self, bitDepth):
-		self.bitdepth = bitDepth
-
-	def convertToGrayscale(self):
-	#REQUIRES: self is an RGB image, can be converted, and OpenCL is all set up
-	#EFFECTS: converts RGB to grayscale image
-		subprocess.call(["./tiff2hdfGrayscaleConvert", self.location])
-	
 	def loadImageFrameToArray(self):
 		img = Image.open(self.location)
-		img.load()
-		self.rawImage = numpy.asarray(img, dtype='uint' + self.bitdepth)
-	
-	def saveImageToBinaryFile(self):
-		self.rawImage.astype(self.bitdepth).tofile(destination)
+		#to get 		
+		self.rawImage = numpy.array(img)
+		#now the image is a numpy array which can be manipulated
+		#http://stackoverflow.com/questions/384759/pil-and-numpy
+		#.seek() will change frames
+
+	def saveAsRawNumpyArray(self):
+	# REQUIRES: rawImage must be set
+	# MODIFIES: disk
+	# EFFECTS: Saves the numpy array to disk with the filename of the iamge
+		numpy.save(self.location, self.rawImage)
+
+
+class fileObject:
+	def __init__(self, name):
+	# REQUIRES: Name be a filename CONTAINING PATH of the HDF5 set to be created.
+	# MODIFIES: self
+		
+		self.name = name
+		self.filename = self.name + 'hdf5'
+		self.width = 0
+		self.height = 0
+		self.dtype = ''
+		self.file = None
+
+	def __del__(self):
+		if self.file != None:
+			self.file.close()
+
+	def checkIfFileExists(self):
+	# REQUIRES: self.filename is set
+	# EFFECTS: Returns true if file to be created already exists
+		if os.path.isfile(self.filename):
+			return true
+		else:
+			return false
+	def createFile(self):
+	# REQUIRES: self.filename is set
+	# EFFECTS: Initializes the self.file object	
+		self.file = h5py.File(self.filename, 'w')
+	def createImageGroup(self):
+	# REQUIRES: self.file is initialized
+	# MODIFIES: self.file
+	# EFFECTS: Creates an image group called "images" within the HDF5 file.
+		self.imageGroup = self.file.create_group("images")
+	def createNewDataset(self, datasetName):
+	# REQUIRES: image group is already created
+	# MODIFIES: self.imageGroup
+	# EFFECTS: Creates a new dataset, and stores it in the self.currentDataset variable
+	# NOTE: This corresponds to a frame in a stack typically
+		self.currentDataset = self.imageGroup.create_dataset(
+			datasetName, 
+			(self.width, self.height),
+			dtype='i' + str(self.bitdepth/8))
+	def setAttribute(self, attributeName, stringContent):
+	#Requires: current group must be set
+		self.imageGroup.attrs[attributeName] = stringContent
+
+	def saveImageToDataset(self, imageToSave):
+	# REQUIRES: Image must be a numpy dataset
+		if self.currentDataset.shape = (imageToSave.width, imageToSave.height)
+			self.currentDataset = imageToSave.rawImage
+		else:
+			print "Dimension mismatch! Exiting..."
+			sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -141,11 +192,11 @@ if __name__ == '__main__':
 		'--raw', 
 		help='Convert TIFFs to raw images, not HDF5 datasets.',
 		action="store_true")
-	parser.add_argument('-c', 
-		'--opencl', 
-		help="""Accelerate RGB to grayscale conversionsion with 
-		OpenCL(requires OpenCL dependencies)""",
-		action="store_true")
+	#parser.add_argument('-c', 
+	#	'--opencl', 
+	#	help="""Accelerate RGB to grayscale conversion with 
+	#	OpenCL(requires OpenCL dependencies)""",
+	#	action="store_true")
 	parser.add_argument('-b', 
 		'--bitdepth', 
 		help="""Convert all TIFF files to HDF5 datasets with N bit integers,
@@ -167,9 +218,32 @@ if __name__ == '__main__':
 		sys.exit("Invalid number of bits set(must be 8,16,32, or 64")
 
 	#checking if openCL C lib is in directory if c flag is set
-	if args.opencl and not os.path.isfile('./libtiff2hdf.so'):
-		sys.exit("Please place the libtiff2hdf.so file in the same directory" +  
-			" with the tiff2hdf.py file if using OpenCL.")
+	#if args.opencl and not os.path.isfile('./libtiff2hdf.so'):
+	#	sys.exit("Please place the libtiff2hdf.so file in the same directory" +  
+	#		" with the tiff2hdf.py file if using OpenCL.")
+	
+	#File conversion routine for one file
+	#Create the image we are going to convert
+	imageToConvert = imageConversion()
+	#Tell the program where the file to convert is
+	imageToConvert.setImageLocation(inputPlace)
+	#Get various details about the image
+	imageToConvert.getImageDetails()
+	#Image must be grayscale, check for this
+	if not imageToConvert.grayscaleCheck:
+		print "ERROR: Image " + imageToConvert.name + " is not grayscale."
+		print "Only grayscale images are supported. Please convert to grayscale and try again."
+		sys.exit(1)
+
+	#We've got to create a file now
+	
+	for framenumber in range(0, imageToConvert.numImages - 1):
+		#grab current frame and put in rawImage array
+		imageToConvert.loadImageFrameToArray()
+
+
+
+
 
 
 
