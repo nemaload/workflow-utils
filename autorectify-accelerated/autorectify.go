9eaaf1db5bf1701d6bc90127fe932ac800385650
Michael Schmatz <schmatz@umich.edu>
2013-05-25 17:00:07 -0700

    progress

--- autorectify-accelerated/autorectify.go
+++ autorectify-accelerated/autorectify.go
@@ -4,11 +4,15 @@
 package main
 
 import "os"
+import "os/exec"
 import "strings"
 import "flag"
 import "fmt"
-import "github.com/sbinet/go-hdf5/pkg/hdf5"
-import "runtime" //http://stackoverflow.com/questions/1739614/what-is-the-difference-between-gos-multithreading-and-pthread-or-java-threads
+import "log"
+import "math"
+
+//import "github.com/sbinet/go-hdf5/pkg/hdf5"
+//import "runtime" //http://stackoverflow.com/questions/1739614/what-is-the-difference-between-gos-multithreading-and-pthread-or-java-threads
 //http://golang.org/doc/effective_go.html#parallel
 
 //Sample optical parameter file
@@ -21,9 +25,12 @@ na 1.4
 medium 1.515
 */
 
+//Optical Parameter Related Things
+
+//OpticsRecipe is a struct designed to store optical parameters of the system.
+//This can either contain the defaults put in by the constructor(copied from LFDisplay),
+//information read from a file, or inputted through standard input.
 type OpticsRecipe struct {
-	//This stores the variables from which maxu is calculated(optical parameters)
-	//This can either be read in from a file or set by the user
 	pitch  float64
 	flen   float64
 	mag    float64
@@ -32,16 +39,9 @@ type OpticsRecipe struct {
 	medium float64
 }
 
-type LensletSettings struct {
-	//This stores the output of autorectify
-	lensletOffset float64
-	lensletHoriz  float64
-	lensletVert   float64
-}
-
 //createOpticsRecipe outputs an initialixed OpticsRecipe
 //struct which contains optical parameters about the optical system.
-func createOpticsRecipe(returnRecipe OpticsRecipe) {
+func createOpticsRecipe() OpticsRecipe {
 	var o OpticsRecipe
 	o.pitch = 125.0
 	o.flen = 2500.0
@@ -72,20 +72,35 @@ func (o *OpticsRecipe) calculateMaxNormalizedSlope() float64 {
 	imagena := o.na / o.mag
 	if imagena < 1.0 {
 		ulenslope := o.pitch / o.flen
-		naslope := imagena / (1.0 - imagena*imagena) * *0.5
-		return naslope / ulensslope
-	} else {
-		return 0.0
+		naslope := imagena / math.Pow((1.0-imagena*imagena), 0.5)
+		return naslope / ulenslope
 	}
+	return 0.0
 }
+
+//End Optical Parameter Related Things
+
+//Autorectification related things
+
+//LensletSettings is a struct designed to hold the results of the autorectification.
+type LensletSettings struct {
+	//This stores the output of autorectify
+	lensletOffset float64
+	lensletHoriz  float64
+	lensletVert   float64
+}
+
+//End Autorectification related things
+
+//Misc. input things
 
 //checkInputCorrectness is a helper routine which validates the input from 
 //fmt.Scan. It returns a boolean value; true if the input was valid, and
 //false if it was not.
 func checkInputCorrectness(numScanned int, numExpected int, inputError error, print bool) bool {
-	if inputError {
+	if inputError != nil {
 		if print {
-			fmt.println("There was an error scanning the input. Please try again.")
+			fmt.Println("There was an error scanning the input. Please try again.")
 		}
 		return false
 	} else if numScanned != numExpected {
@@ -111,6 +126,24 @@ func getInputFloatParameter(description string, printBool bool) float64 {
 	return returnValue
 }
 
+//End Misc. input things
+
+//HDF5 related things
+
+func hdf5_getAttribute(attribute string, group string, filepath string) {
+	//var getAttributeCmd exec.Cmd
+	//getAttributeCmd.Path = "h5dump"
+	//getAttributeCmd.Args = []string{"-a", "/" + group + "/" + attribute}
+	//out, err := getAttributeCmd.Output()
+	out, err := exec.Command("h5dump", "-a", "/"+group+"/"+attribute, filepath).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Output is: %s\n", out)
+}
+
+//End HDF5 related things
+
 //BUG(schmatz): What the abbe value represents is still a mystery. It isn't used so I won't worry too much
 //about it.
 
@@ -118,7 +151,7 @@ func getInputFloatParameter(description string, printBool bool) float64 {
 //HDF5?
 
 func main() {
-
+	hdf5_getAttribute("numFrames", "images", "/Users/schmatz/Epi_63xZeiss_1.4_3beads_w_lenses1.hdf5")
 	//Have function have syntax ./autorectify [-h] [-d] [-m] INPUT OUTPUT
 	displayFlagPtr := flag.Bool("display", false, "Prints autorectification data to screen instead of file")
 	manualFlagPtr := flag.Bool("manual", false, "Takes optical parameters from user instead of defaults")
@@ -137,12 +170,10 @@ func main() {
 		os.Exit(1)
 	}
 	//deal with the optical parameters
-	var opticalParameters OpticsRecipe
+	opticalParameters := createOpticsRecipe()
 	var inputPitch, inputFocalLength, inputMag, inputNA, inputMedium float64
 	var inputAbbe bool
 	if *manualFlagPtr == true {
-		var inputError bool
-		var numScanned int
 		inputPitch = getInputFloatParameter("Please enter the pitch:", true)
 		inputFocalLength = getInputFloatParameter("Please enter the focal length:", true)
 		inputMag = getInputFloatParameter("Please enter the magnification:", true)
@@ -150,7 +181,9 @@ func main() {
 		abbeInputSuccessful := false
 		for abbeInputSuccessful != true {
 			fmt.Println("Is the Abbe/abberation/whatever value true or false?(T/F): ")
-			abbeValue := strings.TrimSpace(strings.ToUpper(fmt.Scanln()))
+			var abbeInputTrueFalse string
+			fmt.Scanln(&abbeInputTrueFalse)
+			abbeValue := strings.TrimSpace(strings.ToUpper(abbeInputTrueFalse))
 			if abbeValue != "T" || abbeValue != "F" {
 				fmt.Println("There was an error parsing your input. Try again.")
 				continue
@@ -159,17 +192,17 @@ func main() {
 				if abbeValue == "T" {
 					inputAbbe = true
 				} else {
-					abbeValue = false
+					inputAbbe = false
 				}
 			}
 		}
 		inputNA = getInputFloatParameter("Please enter the numerical aperture:", true)
-		inputMedium = getInputFloatParameter("Please enter the medium value:", printBool)
+		inputMedium = getInputFloatParameter("Please enter the medium value:", true)
 
 	} else {
 		//now you have to read the input from a file(either text or HDF5?)
 	}
-	OpticsRecipe.setOpticsRecipe(inputPitch, inputFocalLength, inputMag, inputAbbe, inputNA, inputMedium)
+	opticalParameters.setOpticsRecipe(inputPitch, inputFocalLength, inputMag, inputAbbe, inputNA, inputMedium)
 
 	//now that we have the optical parameters we can start doing the fun stuff
 
