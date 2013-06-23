8cd583cddf03ec3e147e1df1584edabc346359e6
Petr Baudis <pasky@ucw.cz>
2013-06-24 01:00:54 +0200

    list_hdf5.py: Simple tool for querying groups stored in HDF5 file

---
+++ hdf-utils/list_hdf5.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# Usage: list_hdf5.py FILENAME
+#
+# Example: ./list_hdf5.py 16_4-15_27.hdf5
+
+import sys
+
+import tables
+
+import numpy
+import scipy.misc
+import matplotlib.pyplot as plt
+
+import json
+
+
+filename = sys.argv[1]
+
+h5file = tables.open_file(filename, mode = "r")
+
+ls = {}
+
+channel_list = [i for (i, node) in h5file.get_node('/', '/images')._v_children.items()]
+for ch in channel_list:
+    group_list = [i for (i, node) in h5file.get_node('/', '/images/' + ch)._v_children.items()]
+    group_list = map(int, group_list)
+    group_list.sort()
+    ls[ch] = group_list
+
+print json.dumps(ls)
