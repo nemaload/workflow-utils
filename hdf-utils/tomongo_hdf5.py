0cb66d8e23d6df9b2d8aaaf55e83bdde133ad9bb
Petr Baudis <pasky@ucw.cz>
2013-07-09 04:14:58 +0200

    tomongo_hdf5.py: New script for importing file-level metadata to a nemashow-compatible Mongo database

---
+++ hdf-utils/tomongo_hdf5.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#
+# Usage: tomongo_hdf5.py FILENAME
+#
+# Example: ./tomongo_hdf5.py 16_4-15_27.hdf5
+
+import os
+import sys
+
+import tables
+import pymongo
+
+
+filename = sys.argv[1]
+basename = os.path.basename(filename)
+
+h5file = tables.open_file(filename, mode = "r")
+
+mongo = pymongo.Connection('localhost', 3002)
+db = mongo.meteor
+Images = db.images
+
+if Images.find({'relPath': basename + '/0/0'}).count() > 0:
+    print 'Duplicate: ' + basename
+    sys.exit(1)
+
+imageObject = {
+    'type': 'ls',
+    'originalPath': os.path.abspath(filename),
+    'baseName': basename,
+    'size': os.stat(filename).st_size,
+    'numFrames': 0,
+    'channels': [],
+    'relPath': []
+}
+
+channel_list = [i for (i, node) in h5file.get_node('/', '/images')._v_children.items()]
+channel_list.sort()
+for ch in channel_list:
+    group_list = [i for (i, node) in h5file.get_node('/', '/images/' + ch)._v_children.items()]
+    group_list = map(int, group_list)
+    group_list.sort()
+
+    chno = ch[len(".ch"):]
+    imageObject['channels'].append(chno)
+
+    imageObject['numFrames'] += len(group_list)
+    imageObject['relPath'].extend([basename + '/' + chno + '/' + str(g) for g in group_list])
+
+print imageObject['relPath'][0] + ' ...'
+print Images.insert(imageObject)
