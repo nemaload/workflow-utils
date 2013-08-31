581284f8b8b3a354eb4d97df0a9ada23890dc2ed
Petr Baudis <pasky@ucw.cz>
2013-08-31 23:38:16 +0200

    hdf-lf-utils/setCropWindow.py: New tool for configuring the cropwindow group

---
+++ hdf-lf-utils/setCropWindow.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# Store a crop window setup with given dimensions in HDF5 file.
+#
+# Usage: setCropWindow.py HDF5FILE X0 Y0 X1 Y1
+#
+# Caution: The images you usually see have the X and Y coordinates
+# swapped compared to what's stored in the dataset! To determine
+# this tool's parameters from values measured on the rotated version
+# (e.g. as shown as nemashow), pass them as:
+#
+#   setCropWindow.py HDF5FILE $((2560-Y1)) X0  $((2160-Y0)) X1
+#
+# (Assuming pre-rotation dataset dimensions 2160x2560.)
+
+import h5py
+import sys
+
+if __name__ == '__main__':
+    h5file = h5py.File(sys.argv[1], 'r+')
+    cropwindowGroup = h5file.require_group("cropwindow")
+    cropwindowGroup.attrs['x0'] = int(sys.argv[2])
+    cropwindowGroup.attrs['y0'] = int(sys.argv[3])
+    cropwindowGroup.attrs['x1'] = int(sys.argv[4])
+    cropwindowGroup.attrs['y1'] = int(sys.argv[5])
+    h5file.close()
