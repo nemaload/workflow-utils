cf390644f93239b8acc909de907ac30e0ba42b37
Petr Baudis <pasky@ucw.cz>
2013-08-31 14:57:49 +0200

    Factor out hdf5lflib.compute_uvframe() from generateUVRaw.processFrameUV()
    It's useful to have in other Python scripts during our BDB+ experiments.

--- hdf-lf-utils/generateUVRaw.py
+++ hdf-lf-utils/generateUVRaw.py
@@ -19,32 +19,10 @@ import hdf5lflib
 gridsize_last = []
 
 def processFrameUV(i, node, outputBase, ofs_U, ofs_V, ar, imgfmt):
-    imgdata = node.read()
-    # scipy.misc.imsave('rawimage.png', imgdata)
+    uvframe = hdf5lflib.compute_uvframe(node, ar, ofs_U, ofs_V)
 
-    gridsize = (int(imgdata.shape[0] / ar._v_attrs['down_dy']), int(imgdata.shape[1] / ar._v_attrs['right_dx']))
     global gridsize_last
-    gridsize_last = gridsize
-    corner = hdf5lflib.lenslets_offset2corner(ar)
-
-    # We also rotate the image by 90\deg during the processing to maintain
-    # compatibility with other parts of our toolchain.
-
-    uvframe = numpy.zeros(shape=(gridsize[1], gridsize[0]), dtype='short')
-
-    (right_dx, right_dy) = ar._v_attrs['right_dx'], ar._v_attrs['right_dy']
-    (down_dx, down_dy) = ar._v_attrs['down_dx'], ar._v_attrs['down_dy']
-
-    for y in range(int(gridsize[0])):
-        for x in range(int(gridsize[1])):
-            cx = corner[1] + x * right_dx + y * down_dx + ofs_U
-            cy = corner[0] + x * right_dy + y * down_dy + ofs_V
-            try:
-                uvframe[gridsize[1]-1 - x][y] = imgdata[int(round(cy))][int(round(cx))]
-                #print cx, cy, gridsize[1]-1 - x, y, int(uvframe[gridsize[1]-1 - x][y])
-            except IndexError:
-                #print cx, cy, gridsize[1]-1 - x, y, '---'
-                pass
+    gridsize_last = uvframe.shape
 
     if imgfmt == 'png':
         scipy.misc.imsave(outputBase + '-' + str(i) + '.png', uvframe)
--- hdf-lf-utils/hdf5lflib.py
+++ hdf-lf-utils/hdf5lflib.py
@@ -1,6 +1,8 @@
 # This is a collection of useful functions used across our Python tools
 # working with HDF5 lightfield files.
 
+import numpy
+
 def compute_maxu(imageGroup):
     """
     Determine the maximum slope for looking through a lens, commonly
@@ -25,6 +27,7 @@ def compute_maxu(imageGroup):
     else:
         maxu = 0.4667937556007068 #calculated from only optics data available at the time, serves as default
     return (maxu, maxu_explicit)
+
 
 def lenslets_offset2corner(ar):
     """
@@ -52,3 +55,37 @@ def lenslets_offset2corner(ar):
     # some room to go many steps up at the cost of going one step right.
 
     return corner
+
+
+def compute_uvframe(node, ar, ofs_U, ofs_V):
+    """
+    Generate a view of the sample from a particular (U,V) viewpoint
+    from the lightsheet data (as stored in HDF5 @node). The view
+    is returned as a numpy 2D array.
+    """
+    imgdata = node.read()
+    # scipy.misc.imsave('rawimage.png', imgdata)
+
+    gridsize = (int(imgdata.shape[0] / ar._v_attrs['down_dy']), int(imgdata.shape[1] / ar._v_attrs['right_dx']))
+    corner = lenslets_offset2corner(ar)
+
+    # We also rotate the image by 90\deg during the processing to maintain
+    # compatibility with other parts of our toolchain.
+
+    uvframe = numpy.zeros(shape=(gridsize[1], gridsize[0]), dtype='short')
+
+    (right_dx, right_dy) = ar._v_attrs['right_dx'], ar._v_attrs['right_dy']
+    (down_dx, down_dy) = ar._v_attrs['down_dx'], ar._v_attrs['down_dy']
+
+    for y in range(int(gridsize[0])):
+        for x in range(int(gridsize[1])):
+            cx = corner[1] + x * right_dx + y * down_dx + ofs_U
+            cy = corner[0] + x * right_dy + y * down_dy + ofs_V
+            try:
+                uvframe[gridsize[1]-1 - x][y] = imgdata[int(round(cy))][int(round(cx))]
+                #print cx, cy, gridsize[1]-1 - x, y, int(uvframe[gridsize[1]-1 - x][y])
+            except IndexError:
+                #print cx, cy, gridsize[1]-1 - x, y, '---'
+                pass
+
+    return uvframe
